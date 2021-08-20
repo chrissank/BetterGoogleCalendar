fed194bec82e88ee6b4ea43c880bf2aca0e54f29
Chris Sankey <csankey6@gmail.com>
2021-08-20 17:50:52 -0400

    add commments, minimize, fix geolocation

--- background-script.js
+++ background-script.js
@@ -1,3 +1,4 @@
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    // any messages sent here will mean to popup the 'turn on geolocation' page, so do so:
     chrome.windows.create({url: chrome.runtime.getURL("src/geolocation.html"), type: "popup", height: 200, width: 400});
 });
--- src/content-script.js
+++ src/content-script.js
@@ -1,19 +1,7 @@
-let lat = 43.6532259;
-let long = -79.383186;
 const exclude = "current,minutely,daily,alerts";
 const appId = "";
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${exclude}&appid=${appId}`;
 
 const sleep = m => new Promise(r => setTimeout(r, m));
-
-async function getWeatherData() {
-    try {
-        const response = await fetch(url);
-        return await response.text();
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 function getCoordinates() {
     return new Promise((res, rej) => {
@@ -22,11 +10,10 @@ function getCoordinates() {
 }
 
 (async () => {
-    let weatherData = JSON.parse(await getWeatherData());
-    console.log(weatherData);
+    let lat = 43.6532259; // default Toronto lat
+    let long = -79.383186; // default Toronto long
 
-    await sleep(500);
-
+    // get user location for weather in users area
     if (navigator.geolocation) {
         try {
             const position = await this.getCoordinates();
@@ -34,60 +21,59 @@ function getCoordinates() {
             lat = position.coords.latitude;
             long = position.coords.longitude;
         } catch (error) {
+            // geolocation not enabled, tell them by sending popup to background-script.js
             chrome.runtime.sendMessage({error: "geolocation"});
             return;
         }
     } else {
+        // geolocation not enabled, tell them by sending popup to background-script.js
         chrome.runtime.sendMessage({error: "geolocation"});
         return;
     }
 
-    const nodes = document.getElementsByTagName("*");
+    // get the the hourly weather report given the current location
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${exclude}&appid=${appId}`;
+    const response = await fetch(url);
+    let weatherData = JSON.parse(await response.text());
 
-    let node;
+    await sleep(500); // let gcal finish loading.. not the best way, but for now, it works on decent internet
 
-    for (var i = 0; i < nodes.length; i++) {
-        if (nodes[i].childElementCount == 25) {
-            if (nodes[i].children[0].childElementCount != 0) {
-                node = nodes[i];
-                break;
-            }
-        }
-    }
-
+    // get the columns shown (only works for 2, 3 or 4 days)
     let columns = document.querySelectorAll("div[role='gridcell'][jsname]");
 
-    let startHour = new Date().getHours();
+    let hour = new Date().getHours();
+    let column = 0;
 
-    let startColumn = 0;
-
+    // which column are we currently looking at
     for (let i = 0; i < columns.length; i++) {
         let column = columns[i];
         if (column.children[0].innerHTML.endsWith(new Date().getDate()) || column.children[0].innerHTML.endsWith("today")) {
-            startColumn = i;
+            column = i;
             break;
         }
     }
 
-    console.log(columns[startColumn].querySelectorAll("div[role='presentation']")[0]);
-
+    // for each entry in the weather data
     for (let i = 0; i < weatherData.hourly.length; i++) {
+        // create a div with the following properties
         const newContent = document.createElement("div");
         newContent.innerHTML = "<i>" + weatherData.hourly[i].weather[0].description + " / " + Math.round(weatherData.hourly[i].temp - 273) + "°C" + "</i>";
-        newContent.style.top = (columns[0].offsetHeight / 24) * startHour + "px";
+        newContent.style.top = (columns[0].offsetHeight / 24) * hour + "px";
         newContent.style.position = "absolute";
         newContent.style.color = "gray";
         newContent.style.fontSize = "10px";
         newContent.dataset.isWeather = true;
 
-        columns[startColumn].querySelectorAll("div[role='presentation']")[0].appendChild(newContent);
+        // add it to the column
+        columns[column].querySelectorAll("div[role='presentation']")[0].appendChild(newContent);
 
-        startHour++;
-        if (startHour >= 24) {
-            startColumn++;
-            startHour = 0;
+        // move to next position as described
+        hour++;
+        if (hour >= 24) {
+            column++;
+            hour = 0;
 
-            if (startColumn == columns.length) break;
+            if (column == columns.length) break;
         }
     }
 
@@ -97,6 +83,7 @@ function getCoordinates() {
         subtree: true,
     };
 
+    // google calendar likes to refresh often, use this to track when the refresh happens
     const observer = new MutationObserver(changeCallback);
     observer.observe(columns[0], observerOptions);
     observer.observe(columns[1], observerOptions);
@@ -107,16 +94,15 @@ function changeCallback(mutationList, observer) {
     mutationList.forEach(mutation => {
         switch (mutation.type) {
             case "childList":
-                //console.log("test1 " + mutation.removedNodes.length);
+                // check if any of our nodes were deleted
                 mutation.removedNodes.forEach(node => {
                     if (node.dataset != null && node.dataset.isWeather) {
-                        //console.log("TESTESTSETEST");
+                        // if our node was deleted, re-add it
                         mutation.target.appendChild(node);
                     }
                 });
                 break;
             case "attributes":
-                //console.log("test2");
                 break;
         }
     });
